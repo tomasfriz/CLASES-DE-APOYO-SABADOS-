6d93411fba5821a7f1b1f5b116849d733c7522fe
tomasfriz <36195779+tomasfriz@users.noreply.github.com>
2019-05-04 12:16:00 -0300

    proyecto del profe - practica del parcial 2.0

---
+++ deUnoAMuchosPeliculas_v2.0/deUnoAMuchosPeliculas/peliculas.c
@@ -0,0 +1,75 @@
+#include "estudios.h"
+#include "peliculas.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicializarPelicula(ePelicula lista[], int tam)
+{
+    int i;
+    for(i=0; i<tam; i++)
+    {
+        lista[i].id = 0;
+        lista[i].estado = LIBRE;
+    }
+}
+
+void harcodearPelicula(ePelicula lista[])
+{
+    int i;
+    int tam = 5;
+    char titulos[][50]= {"capitan america","titanic","avatar","el padrino","iron man"};
+    char director[][50]= {"steve","di caprio","peter","john","tony stark"};
+    int estudios[] = {1,1,3,4,4};
+    int anioDeEstreno[]= {1934,1954,1952,1943,1935};
+    float duracion[]= {134,100,95,120,135};
+
+    for(i=0; i<tam; i++)
+    {
+        lista[i].id = i + 1;
+        strcpy(lista[i].titulo, titulos[i]);
+        strcpy(lista[i].director, director[i]);
+        lista[i].idEstudio = estudios[i];
+        lista[i].anioDeEstreno = anioDeEstreno[i];
+        lista[i].duracion = duracion[i];
+        lista[i].estado = OCUPADO;
+    }
+}
+
+void mostrarListaPeliculas(ePelicula lista[], int tam)
+{
+    int i;
+    printf("------------------------------\n");
+    for(i=0; i<tam; i++)
+    {
+        if(lista[i].estado!=LIBRE)
+        {
+            mostrarPelicula(lista[i]);
+        }
+    }
+    printf("------------------------------\n");
+}
+void mostrarPelicula(ePelicula unaPelicula)
+{
+    printf("%d %10s %10s %d %f\n", unaPelicula.id, unaPelicula.titulo, unaPelicula.director, unaPelicula.anioDeEstreno, unaPelicula.duracion);
+}
+
+void mostrarPelicilasConEstudio(ePelicula listaPeliculas[] , int tampeli, eEstudio listaEstudios[], int tamest)
+{
+    int i;
+    int j;
+    for(i=0; i<tampeli; i++)
+    {
+        if(listaPeliculas[i].estado==OCUPADO) {
+            mostrarPelicula(listaPeliculas[i]);
+
+            for(j=0; j<tamest; j++)
+            {
+                if( listaPeliculas[i].idEstudio == listaEstudios[j].id && listaEstudios[j].estado == OCUPADO)
+                {
+                    mostrarEstudio(listaEstudios[j]);
+                }
+            }
+        }
+    }
+}
---
+++ deUnoAMuchosPeliculas_v2.0/deUnoAMuchosPeliculas/peliculas.h
@@ -0,0 +1,25 @@
+#ifndef PELICULAS_H_INCLUDED
+#define PELICULAS_H_INCLUDED
+
+#define LIBRE 0
+#define OCUPADO 1
+
+typedef struct
+{
+    int id;
+    int estado;
+    char titulo[50];
+    char director[50];
+    int idEstudio;
+    float duracion;
+    int anioDeEstreno;
+
+} ePelicula;
+#include "estudios.h"
+#endif // PELICULAS_H_INCLUDED
+
+void inicializarPelicula(ePelicula lista[], int tam);
+void harcodearPelicula(ePelicula lista[]);
+void mostrarListaPeliculas(ePelicula lista[], int tam);
+void mostrarPelicula(ePelicula unaPelicula);
+void mostrarPelicilasConEstudio(ePelicula listaPeliculas[] , int tampeli, eEstudio listaEstudios[], int tamest);
